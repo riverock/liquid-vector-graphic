cd863af73581174037fbe94ce581f2ecc25b01c9
JD Guzman <jdguzman@mac.com>
2016-10-07 15:13:59 -0400

    remove dead code

--- lib/liquid_vector_graphic/tags/query_tag.rb
+++ lib/liquid_vector_graphic/tags/query_tag.rb
@@ -3,14 +3,6 @@ module LiquidVectorGraphic
     class QueryTag < FormTag
       tag_name :query_field
 
-      # def form_value
-      #   if (method = form_tag_options.delete(:method)) && raw_value.present?
-      #     verify_and_call(method.to_sym)
-      #   else
-      #     super
-      #   end
-      # end
-
       private
 
       def default_date_format
