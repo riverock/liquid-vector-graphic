28958951fd49cb0eb7ff6ee0cb64c277f1e70ee9
JD Guzman <jdguzman@mac.com>
2016-10-07 15:12:51 -0400

    add ability to pass in format string or use default

--- lib/liquid_vector_graphic/tags/form_tag.rb
+++ lib/liquid_vector_graphic/tags/form_tag.rb
@@ -6,9 +6,10 @@ module LiquidVectorGraphic
     class FormTag < Solid::Tag
       tag_name :form_field
 
-      attr_accessor :form_tag_options
+      attr_accessor :form_tag_options, :strftime_string
 
       def display(form_tag_options)
+        self.strftime_string = form_tag_options.delete(:strftime)
         self.form_tag_options = form_tag_options
 
         add_to_form_stack!
@@ -29,7 +30,9 @@ module LiquidVectorGraphic
       end
 
       def form_value
-        if (tag_source = form_tag_options[:source])
+        if (method = form_tag_options.delete(:method)) && raw_value.present?
+          verify_and_call(method.to_sym)
+        elsif (tag_source = form_tag_options[:source])
           id = form_values[current_tag_name] || form_tag_options[:default]
           if (drop = find_source_value(tag_source, id))
             current_context.merge({current_tag_name => drop})
@@ -74,19 +77,19 @@ module LiquidVectorGraphic
       end
 
       def past_date
-        calculated_date(:prev_day).strftime("%Y-%m-%d")
+        calculated_date(:prev_day).strftime(default_date_format)
       end
 
       def future_date
-        calculated_date(:next_day).strftime("%Y-%m-%d")
+        calculated_date(:next_day).strftime(default_date_format)
       end
 
       def past_datetime
-        calculated_date(:prev_day).strftime("%Y-%m-%dT%H:%M:%SZ")
+        calculated_date(:prev_day).strftime(default_datetime_format)
       end
 
       def future_datetime
-        calculated_date(:next_day).strftime("%Y-%m-%dT%H:%M:%SZ")
+        calculated_date(:next_day).strftime(default_datetime_format)
       end
 
       def raw_value
@@ -101,6 +104,14 @@ module LiquidVectorGraphic
 
       def calculated_date(operation)
         (base_date || Date.today).send(operation, raw_value.to_i)
+      end
+
+      def default_date_format
+        strftime_string || '%m/%d/%Y'
+      end
+
+      def default_datetime_format
+        strftime_string || '%m/%d/%Y at %H:%M'
       end
     end
   end
--- lib/liquid_vector_graphic/tags/query_tag.rb
+++ lib/liquid_vector_graphic/tags/query_tag.rb
@@ -3,12 +3,22 @@ module LiquidVectorGraphic
     class QueryTag < FormTag
       tag_name :query_field
 
-      def form_value
-        if (method = form_tag_options.delete(:method)) && raw_value.present?
-          verify_and_call(method.to_sym)
-        else
-          super
-        end
+      # def form_value
+      #   if (method = form_tag_options.delete(:method)) && raw_value.present?
+      #     verify_and_call(method.to_sym)
+      #   else
+      #     super
+      #   end
+      # end
+
+      private
+
+      def default_date_format
+        '%Y-%m-%d'
+      end
+
+      def default_datetime_format
+        '%Y-%m-%dT%H:%M:%SZ'
       end
     end
   end
